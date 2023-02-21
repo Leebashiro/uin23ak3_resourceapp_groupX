aca9b8d02abb007cafde6834f81e34c1c78734e1
Leebashiro <111729362+Leebashiro@users.noreply.github.com>
2023-02-22 00:42:16 +0100

    Update LocationURL.js

--- resourceapp/src/components/LocationURL.js
+++ resourceapp/src/components/LocationURL.js
@@ -27,7 +27,12 @@ function LocationURL({ setSelectedId }) {
         break;
     }
   }, [location.pathname, setSelectedId]);
-  /*Brukte denne for å finne en metode for å gjøre at når man skriver
+  /*Bruker useLocation hook som henter inn info fra URL. Bruker useEffect som kjører logikken hver gang location eller 
+  setSelectedId endres. setSelectedId er brukt som en prop og kjører den valgte Id basert på URL. Dette skjes via switch-metoden
+  som endrer på den valgte Id via URL. Hvis URL ikke viser til noen valid Id, så er default på setSelectedId -1 og ingenting
+  vil vises på UI. 
+
+  Brukte denne for å finne en metode for å gjøre at når man skriver
   path i URL så kommer opp innholdet. https://stackoverflow.com/questions/69829362/url-checker-with-includes
   Deretter brukte jeg denne for å strukturere useEffect funksjonen
   https://stackoverflow.com/questions/48477037/how-to-use-switch-cases-inside-jsx-reactjs*/ 
